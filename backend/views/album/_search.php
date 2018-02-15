b70694d1c4d7ecc6403ce1ac50fb1c83c2a4f67a
OYBRKyangi <bryus75@gmail.com>
2018-02-15 19:44:25 +0500

    xabar

---
+++ backend/views/album/_search.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\AlbumSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="album-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'name_uz') ?>
+
+    <?= $form->field($model, 'name_ru') ?>
+
+    <?= $form->field($model, 'name_en') ?>
+
+    <?= $form->field($model, 'status') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Reset', ['class' => 'btn btn-default']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
